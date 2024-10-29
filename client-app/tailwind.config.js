bd6347e2adf29779670da50cacb5c41a7594c75e
JakeIsCodingAway <jake.s.held123@gmail.com>
2024-10-28 19:02:47 -0500

    added front end

---
+++ client-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Note the addition of the tsx extension
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
